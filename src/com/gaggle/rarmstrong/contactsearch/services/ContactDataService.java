1b7640526092b54b7d80951a8c03cc9785207c66
Roger Armstrong <33676858+roger-armstrong-sse@users.noreply.github.com>
2021-10-08 16:14:31 -0400

    Delete ContactDataService.java

--- src/com/gaggle/rarmstrong/contactsearch/services/ContactDataService.java
+++
@@ -1,7 +0,0 @@
-package com.gaggle.rarmstrong.contactsearch.services;
-
-public interface ContactDataService {
-	public String GetRecordByID(String id);
-	
-	public String GetRecordsBySearch(String searchTerm);
-}
