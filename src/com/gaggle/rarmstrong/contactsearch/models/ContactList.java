b871630e8c8ca472d27c75e683135c8d2150639d
Roger Armstrong <33676858+roger-armstrong-sse@users.noreply.github.com>
2021-10-08 16:14:45 -0400

    Delete ContactList.java

--- src/com/gaggle/rarmstrong/contactsearch/models/ContactList.java
+++
@@ -1,7 +0,0 @@
-package com.gaggle.rarmstrong.contactsearch.models;
-
-import java.util.ArrayList;
-
-public class ContactList extends ArrayList<Contact> {
-	private static final long serialVersionUID = 1L;
-}
