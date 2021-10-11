b7cfb8e75d202674e5179bd66a4e175922954d9f
Roger Armstrong <33676858+roger-armstrong-sse@users.noreply.github.com>
2021-10-11 11:58:25 -0400

    Fix casing on method signatures

---
+++ ContactSearchService/build/classes/com/gaggle/rarmstrong/contactsearch/ContactService.java
@@ -0,0 +1,8 @@
+package com.gaggle.rarmstrong.contactsearch;
+
+public interface ContactService {
+
+	public String getContactByID(String request);
+	
+	public String getContactsBySearch(String request);
+}
---
+++ ContactSearchService/build/classes/com/gaggle/rarmstrong/contactsearch/ContactServiceImpl.java
@@ -0,0 +1,67 @@
+package com.gaggle.rarmstrong.contactsearch;
+
+import org.json.*;
+
+import com.gaggle.rarmstrong.contactsearch.services.ContactDataService;
+import com.gaggle.rarmstrong.contactsearch.services.ContactDataServiceImpl;
+
+public class ContactServiceImpl implements ContactService {
+	private static ContactDataService _dataService;
+	
+	private static final String NO_MATCH_MESSAGE = "No records matched your search.";
+	private static final String ERROR_MESSAGE = "There was an error accessing the data.";
+	
+	public ContactServiceImpl() {
+		_dataService = new ContactDataServiceImpl();		
+	}
+	
+	public ContactServiceImpl(ContactDataService dataService) {
+		_dataService = dataService;
+	}
+		
+	@Override
+	public String getContactByID(String request) {
+		try {
+			JSONObject obj = new JSONObject(request);
+			String id = obj.getString("id");
+		
+			String response = _dataService.GetRecordByID(id);
+			if (response == null) {
+				return "{ \"error\": \"" + ERROR_MESSAGE + "\" }";
+			}
+		
+			if (response == "{}") {
+				return "{ \"error\": \"" + NO_MATCH_MESSAGE + "\" }";
+			}
+		
+			return response;
+		}
+		catch (Exception e) {
+			return "{ \"error\": \"Bad Request Format\" }";
+		}
+	}
+
+	@Override
+	public String getContactsBySearch(String request) {
+		try {
+			JSONObject obj = new JSONObject(request);
+			String searchTerm = obj.getString("searchTerm");
+		
+			String response = _dataService.GetRecordsBySearch(searchTerm);
+			if (response == null) {
+				return "{ \"error\": \"" + ERROR_MESSAGE + "\" }";
+			}
+		
+			obj = new JSONObject(response);
+			JSONArray arr = obj.getJSONArray("results");
+			if (arr.length() == 0) {
+				return "{ \"error\": \"" + NO_MATCH_MESSAGE + "\" }";
+			}
+		
+			return response;
+		}
+		catch (Exception e) {
+			return "{ \"error\": \"Bad Request Format\" }";
+		}
+	}
+}
